5de888129d20ce52489fb32cb829e9e825545de6
unknown <layadi.mohamed9@gmail.com>
2022-02-01 17:25:42 +0100

    added login session

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,71 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+declare module "express-session" {
+  export interface SessionData {
+    userId: number;
+  }
+}
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  //   const post1 = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post1);
-
   const app = express();
-  app.get("/", (req, res) => {
-    res.send("hello express");
-    console.log(req.url);
-  });
-  app.listen(4000, () => {
-    console.log("server started on localhost:4000");
-  });
+
+  const RedisStore = connectRedis(session);
+  const redisClient = new Redis();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "sqdsqfqsdfdsfddsfgfdhdfhfbcv",
+      resave: false,
+    })
+  );
+
+  // app.get("/", (req, res) => {
+  //   res.send("hello express");
+  //   console.log(req.url);
+  // });
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    // cors: { credentials: true, origin: "https://studio.apollographql.com" },
+    // path: "/graphql",
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
